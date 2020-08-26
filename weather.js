1cbb2bbbf86f696a3fdbe090466713f6e0f601ad
Olumide <volumide42@gmai.com>
2020-08-26 01:25:12 +0100

    change default location to lagos

--- weather.js
+++ weather.js
@@ -4,7 +4,11 @@ document.getElementById('time').innerText = new Date().toDateString()
 
 let button = document.getElementById('button').addEventListener('click', ()=>{
 	let city = document.getElementById('city').value
-	fetch(`/call/weather?region=${city}`, {
+	weather(city)
+})
+
+function weather(location = 'Lagos'){
+	fetch(`/call/weather?region=${location}`, {
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json'
@@ -16,7 +20,7 @@ let button = document.getElementById('button').addEventListener('click', ()=>{
 		// <img src=" http://openweathermap.org/img/wn/10d@2x.png" alt="" srcset="">
 		//  http://openweathermap.org/img/wn/10d@2x.png
 		document.getElementById('temp').innerHTML = Math.floor((+result.main.temp - 32) * 5/9) + '<span>°C </span>'
-		// document.getElementById('icon').innerHTML = ` <img src=" http://openweathermap.org/img/wn/${result.weather[0].icon}.png" alt="" srcset="">`
+		document.getElementById('icon').innerHTML = ` <img src=" http://openweathermap.org/img/wn/${result.weather[0].icon}.png" alt="" srcset="">`
 		document.getElementById('region').innerText = result.name 
 		document.getElementById('country').innerText = result.sys.country 
 		document.getElementById('status').innerText = result.weather[0].main 
@@ -24,4 +28,10 @@ let button = document.getElementById('button').addEventListener('click', ()=>{
 		console.log(result)
 	})
 	.catch(err => console.log(err))
-})
+}
+
+weather()
+
+// window.onload = function(){
+// 	weather()
+// }
